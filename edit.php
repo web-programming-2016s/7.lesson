06bc54ae02cd906aecca0df123e2ed9e86e05e72
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-03-31 12:41:20 +0300

    saving to db

--- edit.php
+++ edit.php
@@ -12,36 +12,64 @@
 		exit(); //dont execute further
 		
 	}else{
+		
 		echo "User want to edit row:".$_GET["edit"];
 		
 		//ask for latest data for single row
 		$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_romil");
-
-		$stmt = $mysql->prepare("SELECT id, recipient, message FROM messages_sample WHERE id=?");
 		
-		echo $mysql->error;
 		
-		//replace the ? mark
-		$stmt->bind_param("i", $_GET["edit"]);
-		
-		//bind result data
-		$stmt->bind_result($id, $recipient, $message);
-		
-		$stmt->execute();
-		
-		//we have only 1 row of data
-		if($stmt->fetch()){
+		// maybe  user wants to update data after clicking the button
+		if(isset($_GET["to"]) && isset($_GET["message"])){
 			
-			//we had data
-			echo $id." ".$recipient." ".$message;
+			echo "User modified data, tries to save";
+			
+			
+			//should be validation?
+			
+			$stmt = $mysql->prepare("UPDATE messages_sample SET recipient=?, message=? WHERE id=?");
+			
+			echo $mysql->error;
+			
+			$stmt->bind_param("ssi", $_GET["to"], $_GET["message"], $_GET["edit"]);
+			
+			if($stmt->execute()){
+				
+				echo "saved successfully"; 
+				
+			}else{
+				echo $stmt->error;
+			}
+			
 			
 		}else{
-			
-			//smth went wrong
-			echo $stmt->error;
-			
-		}
+			//user did not click any buttons yet,
+			//give user latest data from db
+			$stmt = $mysql->prepare("SELECT id, recipient, message FROM messages_sample WHERE id=?");
 		
+			echo $mysql->error;
+			
+			//replace the ? mark
+			$stmt->bind_param("i", $_GET["edit"]);
+			
+			//bind result data
+			$stmt->bind_result($id, $recipient, $message);
+			
+			$stmt->execute();
+			
+			//we have only 1 row of data
+			if($stmt->fetch()){
+				
+				//we had data
+				echo $id." ".$recipient." ".$message;
+				
+			}else{
+				
+				//smth went wrong
+				echo $stmt->error;
+				
+			}
+		}
 		
 		
 	}
@@ -54,7 +82,7 @@
 
 <form method="get">
 
-	<input disabled name="edit" value="<?=$id;?>"><br><br>
+	<input hidden name="edit" value="<?=$id;?>">
 
 	<label for="to">to:* <label>
 	<input type="text" name="to" value="<?php echo $recipient; ?>"><br><br>
